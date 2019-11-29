b1660cae7aaa01e927e88d142182ae9f29046152
xuyuan <2291056859@qq.com>
2019-11-29 22:26:34 +0800

    未暴露webpack

---
+++ src/components/tabBar/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { TarBarContainer } from "./styled";
+import { TabBarRoute } from "router";
+import { withRouter } from "react-router-dom";
+class TabBar extends Component {
+  render() {
+    let { path } = this.props;
+    return (
+      <TarBarContainer>
+        <ul>
+          {TabBarRoute.map(item => (
+            <li
+              key={item.path}
+              onClick={this.handleTo.bind(this, item.path)}
+              className={path === item.path ? "activePath" : ""}
+            >
+              <i className="iconfont">{item.icon}</i>
+              <span>{item.text}</span>
+            </li>
+          ))}
+        </ul>
+      </TarBarContainer>
+    );
+  }
+  handleTo(path) {
+    console.log(path);
+    this.props.history.push(path);
+  }
+}
+export default withRouter(TabBar);
